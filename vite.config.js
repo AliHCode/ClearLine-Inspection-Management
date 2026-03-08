e4300498e401c66b32ddd804a5a9a20fd5654bde
Ali Hussnain <alihussnain3349@gmail.com>
2026-03-09 01:41:54 +0500

    Cleanup deleted public assets from git and update PWA manifest icons

--- vite.config.js
+++ vite.config.js
@@ -20,8 +20,8 @@ export default defineConfig({
                 background_color: '#FAFAFA',
                 display: 'standalone',
                 icons: [
-                    { src: '/icon-192x192.png', sizes: '192x192', type: 'image/png' },
-                    { src: '/icon-512x512.png', sizes: '512x512', type: 'image/png' }
+                    { src: '/favicon.png', sizes: '192x192', type: 'image/png' },
+                    { src: '/dashboardlogo.png', sizes: '512x512', type: 'image/png' }
                 ]
             }
         })
