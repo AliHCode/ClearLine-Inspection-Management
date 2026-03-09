65c2160f45bf054dacc6b4043f088dcdcbee8652
Ali Hussnain <alihussnain3349@gmail.com>
2026-03-09 17:15:15 +0500

    logo optimized

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ export default function Header() {
     return (
         <header className="app-header">
             <div className="header-left" onClick={() => navigate(dashPath)} style={{ padding: '0.25rem 0' }}>
-                <img src="/dashboardlogo.png" alt="ProWay Logo" style={{ height: '50px', objectFit: 'contain' }} />
+                <img src="/dashboardlogo.png" alt="ProWay Logo" className="header-logo-img" style={{ height: '50px', objectFit: 'contain' }} />
             </div>
 
             {/* Project Selector */}
